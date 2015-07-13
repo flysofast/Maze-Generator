a4afbd5ed9a0b3677e547e5de5260fb3bc9663dd
flysofast <lehainamm@gmail.com>
2015-07-13 20:09:17 +0700

    first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace GenMapXeTang
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.numSize = new System.Windows.Forms.NumericUpDown();
+            this.progBar = new System.Windows.Forms.ProgressBar();
+            ((System.ComponentModel.ISupportInitialize)(this.numSize)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(971, 38);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Generate";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // numSize
+            // 
+            this.numSize.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.numSize.Location = new System.Drawing.Point(971, 13);
+            this.numSize.Name = "numSize";
+            this.numSize.Size = new System.Drawing.Size(99, 20);
+            this.numSize.TabIndex = 1;
+            this.numSize.Value = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            // 
+            // progBar
+            // 
+            this.progBar.Location = new System.Drawing.Point(12, 13);
+            this.progBar.Name = "progBar";
+            this.progBar.Size = new System.Drawing.Size(219, 23);
+            this.progBar.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1083, 618);
+            this.Controls.Add(this.progBar);
+            this.Controls.Add(this.numSize);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numSize)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.NumericUpDown numSize;
+        private System.Windows.Forms.ProgressBar progBar;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GenMapXeTang
+{
+    public partial class Form1 : Form
+    {
+        int[,] theMainGrid;
+        int mainGridSize = 15;
+        Random random = new Random();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        /// <summary>
+        /// Quyết định cắt theo chiều ngang hay chiều dọc
+        /// </summary>
+        /// <param name="width">chiều dài ô grid</param>
+        /// <param name="height">chiều rộng ô grid</param>
+        /// <returns>true: cắt ngang, false: cắt dọc</returns>
+        bool willDoHorizontalDivide(int width, int height)
+        {
+            if (width < height)
+            {
+                return true;
+            }
+            else if (width > height)
+            {
+                return false;
+            }
+            else
+            {
+                return random.Next(2) == 0 ? true : false;
+            }
+        }
+        /// <summary>
+        /// Cắt ô
+        /// </summary>
+        /// <param name="x">Vị trí gốc của grid</param>
+        /// <param name="y">Vị trí bắt đầu gốc của grid</param>
+        /// <param name="w">chiều dài</param>
+        /// <param name="h">Chiều cao</param>
+        void devide(int x, int y, int w, int h)
+        {
+            if (progBar.Value < 90)
+            {
+                progBar.Value++;
+            }
+            bool horizontal = willDoHorizontalDivide(w, h);
+            //Nếu ô ngắn hơn 2 thì ko cần cắt nữa
+            if (w < 2 || h < 2) return;
+
+            //Vị trí đường cắt
+            int lineX = x + (horizontal ? 0 : random.Next(w - 2));
+            int lineY = y + (horizontal ? random.Next(h - 2) : 0);
+            //if (lineX == mainGridSize - 1 || lineY == mainGridSize)
+            //{
+            //    int a = 0;
+            //}
+            //Vị trí cửa trên đường cắt
+            int pX = lineX + (horizontal ? random.Next(w) : 0);
+            int pY = lineY + (horizontal ? 0 : random.Next(h));
+
+            //Vector di chuyển
+            int dX = horizontal ? 1 : 0;
+            int dY = horizontal ? 0 : 1;
+
+            //Số lần dịch con trỏ vẽ đường chia cắt
+            int times = horizontal ? w : h;
+
+            int lineXClone = lineX, lineYClone = lineY;
+
+            string chieucat = horizontal ? "ngang" : "doc";
+            Console.WriteLine("Cat theo chieu " + chieucat + " tai vi tri (" + lineX + "," + lineY + "), de cua o (" + pX + "," + pY + ")");
+            //Kẻ đường chia cắt
+            for (int i = 1; i <= times; i++)
+            {
+
+                //Nếu không phải là ô cửa
+                if (lineX != pX || lineY != pY)
+                {
+                    //10:S (dưới), 01:E (phải)
+                    theMainGrid[lineX, lineY] |= (horizontal ? 2 : 1);
+                }
+
+                lineX += dX;
+                lineY += dY;
+            }
+
+
+
+            //Đệ quy
+            int sx, sy, sw, sh;
+            sx = x;
+            sy = y;
+            sw = horizontal ? w : lineXClone - x + 1;
+            sh = horizontal ? lineYClone - y + 1 : h;
+            devide(sx, sy, sw, sh);
+
+            sx = horizontal ? x : lineXClone + 1;
+            sy = horizontal ? lineYClone + 1 : y;
+            sw = horizontal ? w : w - (lineXClone - x + 1);
+            sh = horizontal ? h - (lineYClone - y + 1) : h;
+            devide(sx, sy, sw, sh);
+
+        }
+
+        void showResult()
+        {
+            Console.WriteLine("Ket qua de");
+            for (int i = 0; i < mainGridSize; i++)
+            {
+                for (int j = 0; j < mainGridSize; j++)
+                {
+                    Console.Write(theMainGrid[j, i] + " ");
+                }
+                Console.Write("\n");
+            }
+
+
+        }
+
+        void showMaze()
+        {
+            int cellWidth = 20;
+            int offSet = 50;
+            System.Drawing.Pen myPen;
+            myPen = new System.Drawing.Pen(System.Drawing.Color.Red);
+            System.Drawing.Graphics formGraphics = this.CreateGraphics();
+            formGraphics.Clear(Color.Transparent);
+            formGraphics.DrawLine(myPen, offSet, offSet, offSet + cellWidth * mainGridSize, offSet);
+            formGraphics.DrawLine(myPen, offSet, offSet, offSet, offSet + cellWidth * mainGridSize);
+            formGraphics.DrawLine(myPen, offSet, offSet + cellWidth * mainGridSize, offSet + cellWidth * mainGridSize, offSet + cellWidth * mainGridSize);
+            formGraphics.DrawLine(myPen, offSet + cellWidth * mainGridSize, offSet, offSet + cellWidth * mainGridSize, offSet + cellWidth * mainGridSize);
+            //formGraphics.DrawLine(myPen, 0, 0, 200, 200);
+
+            Console.WriteLine("\n\n");
+            for (int i = 0; i < mainGridSize; i++)
+            {
+                for (int j = 0; j < mainGridSize; j++)
+                {
+                    switch (theMainGrid[j, i])
+                    {
+                        case 1:
+                            //Console.Write("|");
+                            //formGraphics.DrawLine(myPen, 0, 0, 200, 200);
+                            formGraphics.DrawLine(myPen, offSet + (1 + j) * cellWidth, offSet + (i) * cellWidth, offSet + ((1 + j) * cellWidth), offSet + (1 + i) * cellWidth);
+                            break;
+                        case 2:
+                            //Console.Write("_");
+                            formGraphics.DrawLine(myPen, offSet + (j) * cellWidth, offSet + (1 + i) * cellWidth, offSet + ((1 + j) * cellWidth), offSet + (1 + i) * cellWidth);
+                            break;
+                        case 3:
+                            //Console.Write("_|");
+                            formGraphics.DrawLine(myPen, offSet + (1 + j) * cellWidth, offSet + (i) * cellWidth, offSet + ((1 + j) * cellWidth), offSet + (1 + i) * cellWidth);
+                            formGraphics.DrawLine(myPen, offSet + (j) * cellWidth, offSet + (1 + i) * cellWidth, offSet + ((1 + j) * cellWidth), offSet + (1 + i) * cellWidth);
+                            break;
+                        default:
+                            Console.Write(" ");
+                            break;
+                    }
+                }
+                
+            }
+            progBar.Value=100;
+            myPen.Dispose();
+            formGraphics.Dispose();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            progBar.Value = 0;
+            mainGridSize = Convert.ToInt32(numSize.Value);
+            theMainGrid = new int[mainGridSize, mainGridSize];
+            devide(0, 0, mainGridSize, mainGridSize);
+            showResult();
+            showMaze();
+        }
+    }
+}
